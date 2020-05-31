79526b9c2ad29393c33a585a4d7a4710f3c6fc42
Zackery Bayle <Zackery.bayle@gmail.com>
2020-05-30 20:08:14 -0700

    add progress cards

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Progress from "../helpers/progress";
+
 import AboutPicture from "../../../static/assets/images/desertsky.jpg";
 import ProfilePicture from "../../../static/assets/images/round-white.png";
 
@@ -14,12 +16,16 @@ export default function() {
                     backgroundPosition: "center"
                 }} 
             
-            />
+            >
+                <Progress />
+            </div>
 
             <div className="right-column">
                 <img src={ProfilePicture} />
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis ante sit amet orci ultricies hendrerit. Nulla consequat orci vel tortor dictum scelerisque. Quisque nibh urna, feugiat ut lectus ut, facilisis suscipit urna. Nam laoreet tristique arcu a porta. Pellentesque vehicula nisi massa, non vulputate felis mattis et. Fusce vestibulum lacinia urna, vitae vulputate tellus tempus ut. Fusce eget lorem et diam rutrum viverra. Sed pulvinar magna lorem, in lobortis lectus euismod vel. Suspendisse nec iaculis leo. Duis porttitor libero ut pellentesque consectetur. Proin enim ex, rutrum vitae rutrum sit amet, egestas non lorem. Nullam ac augue tincidunt, blandit lacus vitae, convallis lorem. Pellentesque tincidunt nisl odio, mollis ullamcorper dui porta eu. Nam faucibus eros a finibus bibendum. Duis ac purus sed elit malesuada bibendum. Nam gravida, magna ac laoreet accumsan, risus nunc mattis velit, id finibus augue erat id mi.
+                    I'm a Front-End Developer in Phoenix, AZ.<br/>
+                    I have a real passion for UI effects, intuitive & dynamic user experiances.<br/>
+                    I love projects that push me to learn new ways to think about a problem.
                 </p>
             </div>
 
